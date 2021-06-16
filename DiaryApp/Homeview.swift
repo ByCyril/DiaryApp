820b9e2665313cc5cc40c6ba7b1f5454a1daad99
Jessi <jessi@Fenando-6.local>
2021-06-16 12:52:09 -0500

    Added homescreen

---
+++ DiaryApp/Homeview.swift
@@ -0,0 +1,20 @@
+//
+//  Homeview.swift
+//  DiaryApp
+//
+//  Created by Jessi on 6/16/21.
+//
+
+import SwiftUI
+
+struct Homeview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Homeview_Previews: PreviewProvider {
+    static var previews: some View {
+        Homeview()
+    }
+}
